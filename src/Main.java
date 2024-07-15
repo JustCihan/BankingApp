06635cae8fb34857f3508851a292888738154270
Cihan <chn.ysf.gltkn@gmail.com>
2024-07-15 17:25:24 +0300

    Güncelleme

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.security.cert.TrustAnchor;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -7,86 +8,71 @@ public class Main {
         //örnegin sorusu gerekli dosyada txt olarak mevcut
         //Burada java da struct yapısı bilmedigimden degişkenlere sabit bir deger  aticam
         long hesap_bakiye = 1000;
-        int hesap_numarasi=123456789,hesap_sifresi=987654321,hesap_numarasi_kontrol,hesap_sifresi_kontrol;
+        int hesap_numarasi = 123456789, hesap_sifresi = 987654321, hesap_numarasi_kontrol, hesap_sifresi_kontrol;
         int islem_turu;
-        float yatirilacak_miktar,cekilicek_miktar;
+        float yatirilacak_miktar, cekilicek_miktar;
         String islem_devam_kontrol;
-        boolean gris,dondur=true;
+        boolean islem_devam_ediyor_mu = true;
 
         System.out.println("Hesap numarasını giriniz");
         hesap_numarasi_kontrol = scanner.nextInt();
         System.out.println("Hesap şifresini giriniz");
         hesap_sifresi_kontrol = scanner.nextInt();
-        while (dondur){
+        while (islem_devam_ediyor_mu) {
 
-            if (123456789 == hesap_numarasi_kontrol && 987654321 == hesap_sifresi_kontrol){
+            if (123456789 == hesap_numarasi_kontrol && 987654321 == hesap_sifresi_kontrol) {
                 System.out.println("Gris Basarili");
-                gris = true;
 
-
-            }
-            else {
-                System.out.println("Gris Basarisiz");
-                gris = false;
-
-            }
-            if (gris){
                 System.out.println("İslem türünü seçiniz \n 1 - Para Yatırma \n 2-Para Çekme  \n 3-Bakiye Sorgulama");
                 islem_turu = scanner.nextInt();
-                if(islem_turu == 1){
-                    System.out.println("Yatırılıck miktarı giriniz");
-                    yatirilacak_miktar = scanner.nextFloat();
-                    if (yatirilacak_miktar >= 1){
-                        hesap_bakiye += (long) yatirilacak_miktar;
-                        System.out.println("Para yatirma işleminiz basarıyla gerçekleştir yeni hesap bakiyeniz "+hesap_bakiye);
 
-                    }else {
-                        System.out.println("Geçerli bir deger giriniz");
 
-                    }
+                switch (islem_turu) {
+                    case 1:
+                        System.out.println("Yatırılıck miktarı giriniz");
+                        yatirilacak_miktar = scanner.nextFloat();
+                        if (yatirilacak_miktar >= 1) {
+                            hesap_bakiye += (long) yatirilacak_miktar;
+                            System.out.println("Para yatirma işleminiz basarıyla gerçekleştir yeni hesap bakiyeniz " + hesap_bakiye);
 
+                        } else {
+                            System.out.println("Geçerli bir deger giriniz");
+
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Çekilicek miktarı giriniz");
+                        cekilicek_miktar = scanner.nextFloat();
+                        if (cekilicek_miktar >= 1 && hesap_bakiye >= cekilicek_miktar) {
+                            hesap_bakiye -= (long) cekilicek_miktar;
+                            System.out.println("Para yatirma işleminiz basarıyla gerçekleştir yeni hesap bakiyeniz " + hesap_bakiye);
+
+                        } else {
+                            System.out.println("Hesap bakiyesi yetersizdir");
+
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Hesap bakiyeniz :" + hesap_bakiye);
+                        break;
 
 
                 }
-                if (islem_turu == 2){
-                    System.out.println("Çekilicek miktarı giriniz");
-                    cekilicek_miktar = scanner.nextFloat();
-                    if (cekilicek_miktar >=1 && hesap_bakiye >= cekilicek_miktar ){
-                        hesap_bakiye -= (long) cekilicek_miktar;
-                        System.out.println("Para yatirma işleminiz basarıyla gerçekleştir yeni hesap bakiyeniz "+hesap_bakiye);
+                scanner.nextLine();
+                System.out.println("Baska bir islem yapmak istiyor musunuz? Lutdfen evet veya hayır'ı tuşlayın: ");
+                String cevap = scanner.nextLine();
+                ;
+                islem_devam_ediyor_mu = cevap.equalsIgnoreCase("evet");
 
-                    }
-                    else{
-                        System.out.println("Hesap bakiyesi yetersizdir");
-
-                    }
-
-
-
-
-                }
-                if (islem_turu == 3){
-                    System.out.println("Hesap bakiyeniz :" +hesap_bakiye);
-
-                }
-
-
-
-            }
-            System.out.println("Baska bir islem yapmak istiyor musunuz? Lutdfen evet veya hayır'ı tuşlayın: ");
-            islem_devam_kontrol = scanner.nextLine();
-            if (Objects.equals(islem_devam_kontrol, "evet")){
-                dondur=true;
-
-            } else if (Objects.equals(islem_devam_kontrol, "hayir")) {
-                dondur =false;
-
-            }
-            else {
-                System.out.println("Hatali bir karakter kullndiniz");
+            } else {
+                System.out.println("Gris Basarisiz");
+                islem_devam_ediyor_mu = false;
             }
 
 
         }
+        scanner.close();
+
+
     }
 }
